4555d644c30c2138c0465cf4ca5b0f1721bd60c7
omjambon <omjambon@gmail.com>
2022-04-03 09:55:43 +0200

    Lecture d'un fichier c++ en python

--- test_ond.py
+++ test_ond.py
@@ -16,12 +16,26 @@ atest = pywt.scale2frequency('db2', 8, precision=8)
 
 
 print('World')
+print(pywt.scale2frequency('gaus1', [273]) / 40e6)
 
 signal_bw = 160  # MHz
 fft_len = 2**10
 
-# Load from cfloat file
-sig0 = np.fromfile(r"C:\Users\HP\Desktop\wavelet\bpsk_in_noise.txt", dtype=np.complex64)
+# Load from cfloat fie
+sig0 = np.fromfile(r"C:\Users\HP\Desktop\wavelet\pasnoisy.iq", dtype=np.double)
+za=[]
+zb=[]
+zz = []
+for i in range(0,len(sig0)):
+    if i  % 2 != 0:
+        zb.append(sig0[i])
+    else:
+        za.append(sig0[i])
+        
+zza = np.array(za)
+zzb = np.array(zb)
+zzz = zza + 1j*zzb
+sig0 = zzz
 plt.specgram(sig0,Fs=40e6)
 plt.show()
 #sig0 = sig0.astype(np.complex64) # Convert to 64
@@ -44,14 +58,21 @@ xf = np.fft.fftfreq(len(sig0))
 # =============================================================================
 
 #from scipy
-widths = np.arange(2, 10000)
-scales = [2**8]  # Wavelet scales
+witdhs = np.arange(2, 10000)
+scales = [2**4, 2**6, 2**8]  # Wavelet scales
 
 # cwtmatr = signal.cwt(sig0, signal.morlet, widths)
 Zxx = (np.fft.fft(sig0))
 freqs = np.square(np.abs(Zxx))
-Wx, scales = cwt(freqs/np.max(freqs), 'morlet')
+Wx, scaless = cwt(freqs/np.max(freqs), 'morlet')
  
-imshow(Wx, yticks=scales, abs=1,
+
+[cfs, frequencies] = pywt.cwt(freqs/np.max(freqs), scales, 'gaus1')
+
+imshow(cfs*np.abs(cfs), yticks=scales, abs=1,
+       title="abs(CWT) | Morlet wavelet",
+       ylabel="scales", xlabel="samples")
+
+imshow(Wx, yticks=scaless, abs=1,
        title="abs(CWT) | Morlet wavelet",
        ylabel="scales", xlabel="samples")
